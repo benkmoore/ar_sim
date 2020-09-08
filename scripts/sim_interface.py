360b5b6fc59390c2d47cc3cc01fe7951c3d15cda
benkmoore <ben.karl.moore@gmail.com>
2020-09-08 16:50:03 +0100

    fixed subscribing to new cov data

--- scripts/sim_interface.py
+++ scripts/sim_interface.py
@@ -9,7 +9,7 @@ import sys
 from ar_commander.msg import ControllerCmd, Decawave
 from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import Pose2D
-from std_msgs.msg import Float64MultiArray, Float64
+from std_msgs.msg import Float64
 from tf.transformations import euler_from_quaternion
 
 sys.path.append(rospy.get_param("AR_SIM_DIR"))
@@ -21,6 +21,7 @@ import configs.robot_v1 as rcfg
 # take model states msg and publish a curr pose in Pose2D format
 
 class SimInterface():
+
     def __init__(self):
         # gazebo model states to Pose2D
         rospy.init_node('sim_interface')
@@ -29,8 +30,8 @@ class SimInterface():
         self.pose = Pose2D()
         self.pos = None
         self.theta = None
-        self.cov1 = None
-        self.cov2 = None
+        self.cov_pos1 = None
+        self.cov_pos2 = None
         self.cov_theta = None
 
         # previous measurements
@@ -74,10 +75,12 @@ class SimInterface():
         self.W_cmd = msg.omega_arr.data
         self.phi_cmd = msg.phi_arr.data
 
+
     def modelStates2Pose2D(self):
         self.pose.x = self.pos.x
         self.pose.y = self.pos.y
         self.pose.theta = self.theta # sim outputs [-pi, pi]
+
 
     def model_statesCallback(self, msg):
         try:
@@ -87,12 +90,12 @@ class SimInterface():
             _, _, self.theta = euler_from_quaternion([self.q.x, self.q.y, self.q.z, self.q.w])
             self.modelStates2Pose2D()
 
-            # publish noisy localization data and transform to end of robot arms
+            # sim noisy localization data from decawave sensors
             loc_data = self.getLocalizationData()
-            print(loc_data)
             self.decawave_pub.publish(loc_data)
-        except ValueError as e:
-            print(e)
+        except ValueError:
+            pass
+
 
     def getLocalizationData(self):
         loc = Decawave()
@@ -104,13 +107,14 @@ class SimInterface():
         loc.y2.data = self.pose.y + rcfg.L*np.sin(self.theta) + np.random.uniform(-self.pos_noise, self.pos_noise)
         loc.theta.data = self.pose.theta + np.random.uniform(-self.theta_noise, self.theta_noise)
 
-        # covariances
         self.findlocalizationCovs(loc.x1.data, loc.y1.data, loc.x2.data, loc.y2.data, loc.theta.data)
-        loc.cov1.data = self.cov1
-        loc.cov2.data = self.cov2
-        loc.cov_theta.data = self.cov_theta
+        if self.cov_pos1 is not None and self.cov_pos2 is not None and self.cov_theta is not None:
+            loc.cov1.data = self.cov_pos1.reshape(-1)
+            loc.cov2.data = self.cov_pos2.reshape(-1)
+            loc.cov_theta.data = self.cov_theta
 
         return loc
+
 
     def findlocalizationCovs(self, x1, y1, x2, y2, theta):
         pos1 = np.array([x1, y1])
@@ -121,8 +125,9 @@ class SimInterface():
         self.pos1_q.append(pos1)
         self.pos2_q.append(pos2)
 
-        self.cov1 = np.cov(np.asarray(self.pos1_q).T)
-        self.cov2 = np.cov(np.asarray(self.pos2_q).T)
+        if len(self.pos1_q) > 1 and len(self.pos2_q) > 1: # avoid true_divide error
+            self.cov_pos1 = np.cov(np.asarray(self.pos1_q).T)
+            self.cov_pos2 = np.cov(np.asarray(self.pos2_q).T)
 
         if self.pos1_prev is None or self.pos2_prev is None or self.theta_prev is None:
             delta_pos1 = delta_pos2 = np.ones(2)
@@ -131,35 +136,41 @@ class SimInterface():
             delta_pos1 = pos1 - self.pos1_prev
             delta_pos2 = pos2 - self.pos2_prev
             delta_theta = theta - self.theta_prev
-
         dth_dp1 = abs(delta_theta/delta_pos1)
         dth_dp2 = abs(delta_theta/delta_pos2)
-        self.cov_theta = npl.multi_dot((dth_dp1, self.cov1, dth_dp1)) \
-                                + npl.multi_dot((dth_dp2, self.cov2, dth_dp2))
+
+        if self.cov_pos1 is not None and self.cov_pos2 is not None:
+            self.cov_theta = npl.multi_dot((dth_dp1, self.cov_pos1, dth_dp1)) \
+                                + npl.multi_dot((dth_dp2, self.cov_pos2, dth_dp2))
 
         # update previous measurements
         self.pos1_prev = pos1
         self.pos2_prev = pos2
         self.theta_prev = theta
 
+
+    def publishSimMsgs(self):
+        # Publish pose to GNC
+        self.pose_pub.publish(self.pose)
+
+        # Publish GNC cmds to sim joints
+        if self.phi_cmd != None and self.W_cmd != None:
+            self.cmdP1_pub.publish(self.phi_cmd[2]) # yaw_joint_l1 -> inner on X axis arm
+            self.cmdP2_pub.publish(self.phi_cmd[3]) # yaw_joint_l2 -> outer on X axis arm
+            self.cmdP3_pub.publish(self.phi_cmd[0]) # yaw_joint_r1 -> inner on Y axis arm
+            self.cmdP4_pub.publish(self.phi_cmd[1]) # yaw_joint_r2 -> outer on Y axis arm
+            self.cmdV5_pub.publish(self.W_cmd[2])   # drive_joint_l1 -> inner on X axis arm
+            self.cmdV6_pub.publish(self.W_cmd[3])   # drive_joint_l2 -> outer on X axis arm
+            self.cmdV7_pub.publish(self.W_cmd[0])   # drive_joint_r1 -> inner on Y axis arm
+            self.cmdV8_pub.publish(self.W_cmd[1])   # drive_joint_r2 -> outer on Y axis arm
+
+
     def run(self):
         rate = rospy.Rate(10) # 10 Hz
         while not rospy.is_shutdown():
-            # Publish pose to GNC
-            self.pose_pub.publish(self.pose)
-
-            # Publish GNC cmds to sim joints
-            if self.phi_cmd != None and self.W_cmd != None:
-                self.cmdP1_pub.publish(self.phi_cmd[2]) # yaw_joint_l1 -> inner on X axis arm
-                self.cmdP2_pub.publish(self.phi_cmd[3]) # yaw_joint_l2 -> outer on X axis arm
-                self.cmdP3_pub.publish(self.phi_cmd[0]) # yaw_joint_r1 -> inner on Y axis arm
-                self.cmdP4_pub.publish(self.phi_cmd[1]) # yaw_joint_r2 -> outer on Y axis arm
-                self.cmdV5_pub.publish(self.W_cmd[2])   # drive_joint_l1 -> inner on X axis arm
-                self.cmdV6_pub.publish(self.W_cmd[3])   # drive_joint_l2 -> outer on X axis arm
-                self.cmdV7_pub.publish(self.W_cmd[0])   # drive_joint_r1 -> inner on Y axis arm
-                self.cmdV8_pub.publish(self.W_cmd[1])   # drive_joint_r2 -> outer on Y axis arm
-
+            self.publishSimMsgs()
             rate.sleep()
+
 
 
 if __name__ == '__main__':
