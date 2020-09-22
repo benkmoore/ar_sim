3e09fcc4970c1b9604295ea0d331d7c0dfc48f2f
benkmoore <ben.karl.moore@gmail.com>
2020-09-22 16:44:53 +0100

    updated covariances with normal distributions based on estiamtor data analysis

--- scripts/sim_interface.py
+++ scripts/sim_interface.py
@@ -19,11 +19,11 @@ import configs.robot_v1 as rcfg
 
 # take model states msg and publish a curr pose in Pose2D format
 
-class SimInterface():
 
+class SimInterface:
     def __init__(self):
         # gazebo model states to Pose2D
-        rospy.init_node('sim_interface')
+        rospy.init_node("sim_interface")
 
         # localization variables & covariances
         self.pose = Pose2D()
@@ -43,40 +43,57 @@ class SimInterface():
         self.theta_noise = params.theta_noise
 
         # publishers
-        self.pose_pub = rospy.Publisher('pose', Pose2D, queue_size=10)
-        self.decawave_pub = rospy.Publisher('sensor/decawave_measurement', Decawave, queue_size=10)
+        self.pose_pub = rospy.Publisher("pose", Pose2D, queue_size=10)
+        self.decawave_pub = rospy.Publisher(
+            "sensor/decawave_measurement", Decawave, queue_size=10
+        )
 
-        self.cmdP1_pub = rospy.Publisher("joint1_position_controller/command",Float64, queue_size=10)
-        self.cmdP2_pub = rospy.Publisher("joint2_position_controller/command",Float64, queue_size=10)
-        self.cmdP3_pub = rospy.Publisher("joint3_position_controller/command",Float64, queue_size=10)
-        self.cmdP4_pub = rospy.Publisher("joint4_position_controller/command",Float64, queue_size=10)
-        self.cmdV5_pub = rospy.Publisher("joint5_velocity_controller/command",Float64, queue_size=10)
-        self.cmdV6_pub = rospy.Publisher("joint6_velocity_controller/command",Float64, queue_size=10)
-        self.cmdV7_pub = rospy.Publisher("joint7_velocity_controller/command",Float64, queue_size=10)
-        self.cmdV8_pub = rospy.Publisher("joint8_velocity_controller/command",Float64, queue_size=10)
+        self.cmdP1_pub = rospy.Publisher(
+            "joint1_position_controller/command", Float64, queue_size=10
+        )
+        self.cmdP2_pub = rospy.Publisher(
+            "joint2_position_controller/command", Float64, queue_size=10
+        )
+        self.cmdP3_pub = rospy.Publisher(
+            "joint3_position_controller/command", Float64, queue_size=10
+        )
+        self.cmdP4_pub = rospy.Publisher(
+            "joint4_position_controller/command", Float64, queue_size=10
+        )
+        self.cmdV5_pub = rospy.Publisher(
+            "joint5_velocity_controller/command", Float64, queue_size=10
+        )
+        self.cmdV6_pub = rospy.Publisher(
+            "joint6_velocity_controller/command", Float64, queue_size=10
+        )
+        self.cmdV7_pub = rospy.Publisher(
+            "joint7_velocity_controller/command", Float64, queue_size=10
+        )
+        self.cmdV8_pub = rospy.Publisher(
+            "joint8_velocity_controller/command", Float64, queue_size=10
+        )
 
         # subscribers
-        rospy.Subscriber('/gazebo/model_states', ModelStates, self.model_statesCallback)
-        rospy.Subscriber('controller_cmds', ControllerCmd, self.control_cmdsCallback)
-
+        rospy.Subscriber("/gazebo/model_states", ModelStates, self.model_statesCallback)
+        rospy.Subscriber("controller_cmds", ControllerCmd, self.control_cmdsCallback)
 
     def control_cmdsCallback(self, msg):
         self.W_cmd = msg.omega_arr.data
         self.phi_cmd = msg.phi_arr.data
 
-
     def modelStates2Pose2D(self):
         self.pose.x = self.pos.x
         self.pose.y = self.pos.y
-        self.pose.theta = self.theta # sim outputs [-pi, pi]
-
+        self.pose.theta = self.theta  # sim outputs [-pi, pi]
 
     def model_statesCallback(self, msg):
         try:
             idx = msg.name.index(rospy.get_namespace()[1:-1])
             self.pos = msg.pose[idx].position
             self.q = msg.pose[idx].orientation
-            _, _, self.theta = euler_from_quaternion([self.q.x, self.q.y, self.q.z, self.q.w])
+            _, _, self.theta = euler_from_quaternion(
+                [self.q.x, self.q.y, self.q.z, self.q.w]
+            )
             self.modelStates2Pose2D()
 
             # sim noisy localization data from decawave sensors
@@ -85,30 +102,41 @@ class SimInterface():
         except ValueError:
             pass
 
-
     def getLocalizationData(self):
         loc = Decawave()
         # sensor on Y axis arm
-        loc.x1.data = self.pose.x - rcfg.L*np.sin(self.theta) + np.random.uniform(-self.pos_noise, self.pos_noise)
-        loc.y1.data = self.pose.y + rcfg.L*np.cos(self.theta) + np.random.uniform(-self.pos_noise, self.pos_noise)
+        loc.x1.data = (
+            self.pose.x
+            - rcfg.L * np.sin(self.theta)
+            + np.random.uniform(-self.pos_noise, self.pos_noise)
+        )
+        loc.y1.data = (
+            self.pose.y
+            + rcfg.L * np.cos(self.theta)
+            + np.random.uniform(-self.pos_noise, self.pos_noise)
+        )
         # sensor on X axis arm
-        loc.x2.data = self.pose.x + rcfg.L*np.cos(self.theta) + np.random.uniform(-self.pos_noise, self.pos_noise)
-        loc.y2.data = self.pose.y + rcfg.L*np.sin(self.theta) + np.random.uniform(-self.pos_noise, self.pos_noise)
+        loc.x2.data = (
+            self.pose.x
+            + rcfg.L * np.cos(self.theta)
+            + np.random.uniform(-self.pos_noise, self.pos_noise)
+        )
+        loc.y2.data = (
+            self.pose.y
+            + rcfg.L * np.sin(self.theta)
+            + np.random.uniform(-self.pos_noise, self.pos_noise)
+        )
         # theta
-        loc.theta.data = self.pose.theta + np.random.uniform(-self.theta_noise, self.theta_noise)
+        loc.theta.data = self.pose.theta + np.random.uniform(
+            -self.theta_noise, self.theta_noise
+        )
 
-        loc.cov1.data = self.covsUniformDist(-self.pos_noise, self.pos_noise, dim=2).reshape(-1)
-        loc.cov2.data = self.covsUniformDist(-self.pos_noise, self.pos_noise, dim=2).reshape(-1)
-        loc.cov_theta.data = self.covsUniformDist(-self.theta_noise, self.theta_noise, dim=1)
+        # covariances: normal distribution: standard deviation^2
+        loc.cov1.data = ((self.pos_noise ** 2) * np.eye(2)).reshape(-1)
+        loc.cov2.data = ((self.pos_noise ** 2) * np.eye(2)).reshape(-1)
+        loc.cov_theta.data = self.theta_noise ** 2
 
         return loc
-
-
-    def covsUniformDist(self, noise_low, noise_high, dim):
-        cov = ((noise_high**3)-(noise_low**3))/(3*(noise_high-noise_low))*np.eye(dim)
-
-        return cov
-
 
     def publishSimMsgs(self):
         # Publish pose to GNC
@@ -116,24 +144,38 @@ class SimInterface():
 
         # Publish GNC cmds to sim joints
         if self.phi_cmd != None and self.W_cmd != None:
-            self.cmdP1_pub.publish(self.phi_cmd[2]) # yaw_joint_l1 -> inner on X axis arm
-            self.cmdP2_pub.publish(self.phi_cmd[3]) # yaw_joint_l2 -> outer on X axis arm
-            self.cmdP3_pub.publish(self.phi_cmd[0]) # yaw_joint_r1 -> inner on Y axis arm
-            self.cmdP4_pub.publish(self.phi_cmd[1]) # yaw_joint_r2 -> outer on Y axis arm
-            self.cmdV5_pub.publish(self.W_cmd[2])   # drive_joint_l1 -> inner on X axis arm
-            self.cmdV6_pub.publish(self.W_cmd[3])   # drive_joint_l2 -> outer on X axis arm
-            self.cmdV7_pub.publish(self.W_cmd[0])   # drive_joint_r1 -> inner on Y axis arm
-            self.cmdV8_pub.publish(self.W_cmd[1])   # drive_joint_r2 -> outer on Y axis arm
-
+            self.cmdP1_pub.publish(
+                self.phi_cmd[2]
+            )  # yaw_joint_l1 -> inner on X axis arm
+            self.cmdP2_pub.publish(
+                self.phi_cmd[3]
+            )  # yaw_joint_l2 -> outer on X axis arm
+            self.cmdP3_pub.publish(
+                self.phi_cmd[0]
+            )  # yaw_joint_r1 -> inner on Y axis arm
+            self.cmdP4_pub.publish(
+                self.phi_cmd[1]
+            )  # yaw_joint_r2 -> outer on Y axis arm
+            self.cmdV5_pub.publish(
+                self.W_cmd[2]
+            )  # drive_joint_l1 -> inner on X axis arm
+            self.cmdV6_pub.publish(
+                self.W_cmd[3]
+            )  # drive_joint_l2 -> outer on X axis arm
+            self.cmdV7_pub.publish(
+                self.W_cmd[0]
+            )  # drive_joint_r1 -> inner on Y axis arm
+            self.cmdV8_pub.publish(
+                self.W_cmd[1]
+            )  # drive_joint_r2 -> outer on Y axis arm
 
     def run(self):
-        rate = rospy.Rate(10) # 10 Hz
+        rate = rospy.Rate(10)  # 10 Hz
         while not rospy.is_shutdown():
             self.publishSimMsgs()
             rate.sleep()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     interface = SimInterface()
     interface.run()
